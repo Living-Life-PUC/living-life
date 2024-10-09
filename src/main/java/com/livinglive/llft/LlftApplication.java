907821984aa9a788d1c4b04cd6e9825732b5a035
gustavopcr <gustavopc2501@gmail.com>
2024-10-09 16:44:55 -0300

    init commit

---
+++ src/main/java/com/livinglive/llft/LlftApplication.java
@@ -0,0 +1,13 @@
+package com.livinglive.llft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/livinglive/llft/LlftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livinglive.llft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
