0661cc59eda806b9bfd255c4ab8c3d65abffa516
gustavopcr <gustavopc2501@gmail.com>
2024-10-29 14:17:11 -0300

    workout repository

--- src/main/java/com/livinglive/llft/reaction/dto/CreateReactionDto.java
+++ src/main/java/com/livinglive/llft/reaction/dto/CreateReactionDto.java
@@ -1,7 +1,6 @@
 package com.livinglive.llft.reaction.dto;
 
 import com.livinglive.llft.reaction.Reaction;
-import java.util.UUID;
 
 public record CreateReactionDto(Reaction.ReactionType reactionType){    
 }
---
+++ src/main/java/com/livinglive/llft/workout/WorkoutRepository.java
@@ -0,0 +1,12 @@
+package com.livinglive.llft.workout;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface WorkoutRepository extends JpaRepository<Workout, Long>{
+    Optional <Workout> findByUsername(String name);
+}
